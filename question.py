8c58f5721a22d5769d0f4ea9ee9e7e47be0c364d
lamia0407 <86952273+lamia0407@users.noreply.github.com>
2021-07-06 16:16:13 +0900

    Update question.py

--- question.py
+++ question.py
@@ -17,7 +17,7 @@ async def on_message(message):
         # 文字から「/box」を抜く
         question = message.content[len('/box'):].strip()
         # 質問させたいチャンネルのid
-        target_channel_id = 527769481926410242
+        target_channel_id = getTargetChannelId()
 
         # id=0なら質問者にエラー報告DM
         # idが0以外なら匿名質問する
@@ -32,6 +32,37 @@ async def on_message(message):
             target_channel = client.get_channel(target_channel_id)
             # チャンネルに質問メッセージ送信
             await target_channel.send(question)
+
+
+# 匿名質問させたいチャンネルのidを取得
+# 指定したカテゴリにある最初のTextチャンネル＝質問させたいチャンネルとみなす
+# ただしカテゴリにチャンネルが無い時は0を返す
+def getTargetChannelId() -> int:
+    # 質問させたいチャンネル(対象チャンネル)
+    target_channel = {'id': 0, 'position': 99999999}
+    # ***********************************************************
+    # 指定カテゴリ(対象チャンネルが含まれたカテゴリ)の名前
+
+    category_id = INT_ID_OF_YOUR_CATEGORY  # カテゴリidを指定
+    target_category_name = client.get_channel(category_id).name
+
+    # ***********************************************************
+    # 指定したサーバにある全てのTextチャンネル一覧
+    all_channels = client.get_guild(INT_ID_OF_SERVER).text_channels
+
+    # 全てのTextチャンネルから「指定カテゴリに属する最初のチャンネル」を探す
+    for channel in all_channels:
+        # 指定カテゴリに属する場合だけ対象チャンネル候補とみなす
+        if str(channel.category) == target_category_name:
+            # positionが小さいほうを「より対象チャンネルに近い」として交換
+            # 初期値はpositionが大きい(99999999)ので，必ず入れ替わる想定
+            # 繰り返せば，最後にはpositionが最も小さいチャンネルを代入できる
+            if target_channel['position'] > int(channel.position):
+                target_channel['id'] = int(channel.id)
+                target_channel['position'] = int(channel.position)
+    # 最終的に代入されたidを返す
+    return target_channel['id']
+
 
 # botとしてDiscordに接続(botのトークンを指定)
 client.run('TOKEN_OF_YOUR_BOT')
