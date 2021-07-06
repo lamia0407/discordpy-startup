b1e4c131485b692958c362074ab66963c0333203
lamia0407 <86952273+lamia0407@users.noreply.github.com>
2021-07-06 23:44:45 +0900

    Update question.py

--- question.py
+++ question.py
@@ -17,12 +17,20 @@ async def on_command_error(ctx, error):
 @client.event
 async def on_message(message):
     # 「/box」が頭についたメッセージならオウム返しする
-    if message.content.startswith('/box'):
+    if message.content.startswith(''):
         # 文字から「/box」を抜く
         question = message.content[len('/box'):].strip()
         # 質問させたいチャンネルのid
         target_channel_id = 861529320383578112
 
+        # id=0なら質問者にエラー報告DM
+        # idが0以外なら匿名質問する
+        if target_channel_id == 0:
+            dm = await message.author.create_dm()  # 質問者へDM作成
+            await dm.send(
+                'Sorry, メッセージを送信できませんでした．'
+                'もう1度試してみてください．\n'
+                '【質問文】' + question)
         else:
             # 匿名質問させたいチャンネル
             target_channel = client.get_channel(target_channel_id)
